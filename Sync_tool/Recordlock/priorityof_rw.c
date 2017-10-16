5a31368927b6fb3736bc5e0cf56b603912a45119
Peterhaiker <vim.memory@gmail.com>
2017-10-17 00:51:46 +0800

    增加了记录上锁的练习程序，测试读写的优先级

---
+++ Sync_tool/Recordlock/priorityof_rw.c
@@ -0,0 +1,98 @@
+/*---------------------------------------------
+ *priorityof_rw.c
+ *Copyright (C) 2017-10-16 23:28 
+ *author Peterhaiker 
+ *mail   <vim.memory@gmail.com>
+ *----------------------------------------------
+ *Description:
+ */
+
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<pthread.h>
+#include<errno.h>
+
+int fd;
+void*th1(void*arg)
+{
+    errno=0;
+    struct flock lock;
+    lock.l_type=F_WRLCK;
+    lock.l_whence=SEEK_SET;
+    lock.l_start=0;
+    lock.l_len=0;
+    if(-1==fcntl(fd,F_SETLKW,&lock)){
+        perror("thread 1 set write lock failed");
+        return (void*)errno;
+    }
+    puts("thread 1 set write lock");
+    lock.l_type=F_UNLCK;
+    if(-1==fcntl(fd,F_SETLKW,&lock)){
+        perror("thread 1 unlock write lock failed");
+        return (void*)errno;
+    }
+    return 0;
+}
+
+void*th2(void*arg)
+{
+    errno=0;
+    struct flock lock;
+    lock.l_type=F_RDLCK;
+    lock.l_whence=SEEK_SET;
+    lock.l_start=0;
+    lock.l_len=0;
+    if(-1==fcntl(fd,F_SETLKW,&lock)){
+        perror("thread 2 set read lock failed");
+        return (void*)errno;
+    }
+    puts("thread 2 set read lock");
+    lock.l_type=F_UNLCK;
+    if(-1==fcntl(fd,F_SETLKW,&lock)){
+        perror("thread 2 unlock read lock failed");
+        return (void*)errno;
+    }
+    return 0;
+}
+
+int main(int argc,char*argv[])
+{
+    errno=0;
+    pthread_t tid1,tid2;
+    fd=open("a.txt",O_CREAT|O_RDWR,0644);
+    if(fd<=0){
+        perror("create file failed");
+        return errno;
+    }
+    //now set a read lock to the file
+    struct flock lock;
+    lock.l_type=F_RDLCK;
+    lock.l_whence=SEEK_SET;
+    lock.l_start=0;
+    lock.l_len=0;
+    if(-1==fcntl(fd,F_SETLK,&lock)){
+        perror("main thread set read lock failed");
+        return errno;
+    }
+    puts("main thread set read lock");
+    if(-1==pthread_create(&tid1,NULL,th1,NULL)){
+        perror("create thread 1 failed");
+        return errno;
+    }
+    //sleep 1 second to ensure therad 1 try to get write lock
+    if(-1==pthread_create(&tid2,NULL,th2,NULL)){
+        perror("create thread 2 failed");
+        return errno;
+    }
+    sleep(1);
+    lock.l_type=F_UNLCK;
+    if(-1==fcntl(fd,F_SETLKW,&lock)){
+        perror("main thread unlock read lock failed");
+        return errno;
+    }
+    pthread_join(tid1,NULL);
+    pthread_join(tid2,NULL);
+    return 0;
+}
+
