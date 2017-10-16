b9e185468df8cfa64c6501a5400a5675a7369f98
Peterhaiker <vim.memory@gmail.com>
2017-10-17 01:08:42 +0800

    用多进程来测试记录上锁对于读写的优先级

--- Sync_tool/Recordlock/priorityof_rw.c
+++ Sync_tool/Recordlock/priorityof_rw.c
@@ -9,57 +9,16 @@
 
 #include<stdio.h>
 #include<unistd.h>
+#include<sys/wait.h>
+#include<sys/types.h>
 #include<fcntl.h>
-#include<pthread.h>
 #include<errno.h>
-
-int fd;
-void*th1(void*arg)
-{
-    errno=0;
-    struct flock lock;
-    lock.l_type=F_WRLCK;
-    lock.l_whence=SEEK_SET;
-    lock.l_start=0;
-    lock.l_len=0;
-    if(-1==fcntl(fd,F_SETLKW,&lock)){
-        perror("thread 1 set write lock failed");
-        return (void*)errno;
-    }
-    puts("thread 1 set write lock");
-    lock.l_type=F_UNLCK;
-    if(-1==fcntl(fd,F_SETLKW,&lock)){
-        perror("thread 1 unlock write lock failed");
-        return (void*)errno;
-    }
-    return 0;
-}
-
-void*th2(void*arg)
-{
-    errno=0;
-    struct flock lock;
-    lock.l_type=F_RDLCK;
-    lock.l_whence=SEEK_SET;
-    lock.l_start=0;
-    lock.l_len=0;
-    if(-1==fcntl(fd,F_SETLKW,&lock)){
-        perror("thread 2 set read lock failed");
-        return (void*)errno;
-    }
-    puts("thread 2 set read lock");
-    lock.l_type=F_UNLCK;
-    if(-1==fcntl(fd,F_SETLKW,&lock)){
-        perror("thread 2 unlock read lock failed");
-        return (void*)errno;
-    }
-    return 0;
-}
 
 int main(int argc,char*argv[])
 {
     errno=0;
-    pthread_t tid1,tid2;
+    int fd;
+    pid_t pid1,pid2;
     fd=open("a.txt",O_CREAT|O_RDWR,0644);
     if(fd<=0){
         perror("create file failed");
@@ -75,24 +34,62 @@ int main(int argc,char*argv[])
         perror("main thread set read lock failed");
         return errno;
     }
-    puts("main thread set read lock");
-    if(-1==pthread_create(&tid1,NULL,th1,NULL)){
-        perror("create thread 1 failed");
+    puts("main process set read lock");
+    if((pid1=fork())<0){
+        perror("create process 1 failed");
         return errno;
     }
-    //sleep 1 second to ensure therad 1 try to get write lock
-    if(-1==pthread_create(&tid2,NULL,th2,NULL)){
-        perror("create thread 2 failed");
+    else if(pid1==0){
+        errno=0;
+        lock.l_type=F_WRLCK;
+        lock.l_whence=SEEK_SET;
+        lock.l_start=0;
+        lock.l_len=0;
+        if(-1==fcntl(fd,F_SETLKW,&lock)){
+            perror("child 1 set write lock failed");
+            return errno;
+        }
+        puts("child 1 set write lock");
+        lock.l_type=F_UNLCK;
+        if(-1==fcntl(fd,F_SETLKW,&lock)){
+            perror("child 1 unlock write lock failed");
+            return errno;
+        }
+        return 0;
+    }
+    //sleep 1 second to ensure child 1 try to get write lock
+    sleep(1);
+    if((pid2=fork())<0){
+        perror("create child 2 failed");
         return errno;
     }
+    else if(pid2==0){
+        errno=0;
+        lock.l_type=F_RDLCK;
+        lock.l_whence=SEEK_SET;
+        lock.l_start=0;
+        lock.l_len=0;
+        if(-1==fcntl(fd,F_SETLKW,&lock)){
+            perror("child 2 set read lock failed");
+            return errno;
+        }
+        puts("child 2 set read lock");
+        lock.l_type=F_UNLCK;
+        if(-1==fcntl(fd,F_SETLKW,&lock)){
+            perror("child 2 unlock read lock failed");
+            return errno;
+        }
+        return 0;
+    }
+    //ensure child 2 run first
     sleep(1);
     lock.l_type=F_UNLCK;
     if(-1==fcntl(fd,F_SETLKW,&lock)){
         perror("main thread unlock read lock failed");
         return errno;
     }
-    pthread_join(tid1,NULL);
-    pthread_join(tid2,NULL);
+    while(-1!=waitpid(-1,NULL,WNOHANG))
+        ;
     return 0;
 }
 
