ee722c0ee1e3905c24b9f108248e1093851571b0
Peterhaiker <vim.memory@gmail.com>
2017-10-16 12:45:33 +0800

    读写锁设置为读优先，并改写了测试程序

--- Sync_tool/Rwlock/testmylock.c
+++ Sync_tool/Rwlock/testmylock.c
@@ -13,14 +13,12 @@
 #include"myrwlock.h"
 
 Pthread_rwlock_t rw=PTHREAD_RWLOCK_INITIALIZER;
-pthread_t tid1,tid2;
+pthread_t tid1,tid2,tid3;
 
 void*th1(void*arg)
 {
     Pthread_rwlock_rdlock(&rw);
     puts("th1:get read lock");
-    sleep(3);
-    pthread_cancel(tid2);
     sleep(3);
     Pthread_rwlock_unlock(&rw);
     pthread_exit(NULL);
@@ -34,6 +32,13 @@ void*th2(void*arg)
     pthread_exit(NULL);
 }
 
+void*th3(void*arg)
+{
+    Pthread_rwlock_rdlock(&rw);
+    puts("th3:get read lock");
+    Pthread_rwlock_unlock(&rw);
+    pthread_exit(NULL);
+}
 int main(int argc,char*argv[])
 {
     if(pthread_create(&tid1,NULL,th1,NULL)!=0){
@@ -45,10 +50,11 @@ int main(int argc,char*argv[])
         perror("create thread 2 failed");
         return 0;
     }
+    if(pthread_create(&tid3,NULL,th3,NULL)!=0){
+        perror("create thread 3 failed");
+        return 0;
+    }
     pthread_join(tid2,NULL);
-    puts("th2 canceled");
-    pthread_join(tid1,NULL);
-    puts("th1 returned");
     return 0;
 }
 
